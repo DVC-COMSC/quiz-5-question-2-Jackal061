89bb9b97269139836b54a00e15ee9bd35adda53b
Jackal061 <jordanmacle2005@gmail.com>
2024-03-08 22:42:14 -0800

    variables created

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main()
 {
    int N, M;
+   int result = 1;
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
    /* Complete your code */
+
   
 }
